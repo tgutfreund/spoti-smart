f1f0d43027d8849dc4a2723bceef2d83932a4f1e
Tal <tal.gutfreund@gmail.com>
2025-10-15 23:12:53 +0300

    Refactor SpotifyClient authentication to use file-based caching and remove memory cache handler

--- src/spotify_client.py
+++ src/spotify_client.py
@@ -25,8 +25,6 @@ from spotipy.oauth2 import SpotifyOAuth
 import os
 from dotenv import load_dotenv
 
-from spotipy.cache_handler import MemoryCacheHandler 
-
 # Load environment variables from .env file
 load_dotenv()
 
@@ -74,14 +72,13 @@ class SpotifyClient:
             spotipy.Spotify: Authenticated Spotify client, or None if failed
         """
         try:
-            cache_handler = MemoryCacheHandler()
             # Set up OAuth2 authentication manager
             auth_manager = SpotifyOAuth(
                 client_id=self.client_id,
                 client_secret=self.client_secret,
                 redirect_uri=self.redirect_uri,
                 scope=self.scope,
-                cache_handler=cache_handler
+                cache_path=".spotify_cache"
             )
             
             # Create authenticated Spotify client
