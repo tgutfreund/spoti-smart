714972670c6f7d79aaf50c017c49fc0ff1ed6973
Tal <tal.gutfreund@gmail.com>
2025-10-15 12:22:05 +0300

    Remove unused imports from app.py to streamline the code

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import plotly.express as px
-import plotly.graph_objects as go
-from collections import Counter
-import time
 import sys
 import os
 
